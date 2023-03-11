023ea1fbf56a2c3ca55a6c0bf5adb15c5bebe051
omairi <47171006+omairi@users.noreply.github.com>
2023-03-11 03:11:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('my Parents new healthy dinner')
+streamlit.header('Break fast menu')
+streamlit.text('Kale spinach')
+streamlit.text('Oakra Inc')
+streamlit.text('Bread with Jam')
+
