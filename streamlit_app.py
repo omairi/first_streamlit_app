4ed2db8b327ca021c4fab95a714b87388a30b439
omairi <47171006+omairi@users.noreply.github.com>
2023-03-11 03:31:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import pandas
 import streamlit
 streamlit.title('my Parents new healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌Breakfast Menu')
+streamlit.text('🥭Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍇Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥝Hard-Boiled Free-Range Egg')
 
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #my_fruit_list = my_fruit_list.set_index('Fruit')
