27963c1d1f55e31c8d1804203ce2f3a767465292
omairi <47171006+omairi@users.noreply.github.com>
2023-03-13 14:45:41 -0400

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,1 +1,31 @@
+import pandas
+import streamlit
+import requests
+streamlit.title('my Parents new healthy dinner')
+streamlit.header('🍌Breakfast Menu')
+streamlit.text('🥭Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍇Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥝Hard-Boiled Free-Range Egg')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+#streamlit.multiselect("pick from fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
+streamlit.dataframe(fruits_to_show)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+
+
+#fruits_selected=streamlit.multiselect("pick from fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+#streamlit.dataframe(my_fruit_list)
+
+
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
