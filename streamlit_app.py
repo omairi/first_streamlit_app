f1237ecb72b06bd94a3bbf3f3a6daa6e3830ab46
omairi <47171006+omairi@users.noreply.github.com>
2023-03-13 23:23:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,29 +65,29 @@ streamlit.text("Hello from Snowflake:")
 # write your own comment - what does this do?
 #Snowflake related function
 def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur:
-           my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-           return my_cur.fetchall()
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+         return my_cur.fetchall()
 
 # Add a button to load the fruit
 if streamlit.button('Get fruit Load List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows= get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows= get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
      
 #my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 #my_data_row = my_cur.fetchone()
 #streamlit.text("The Fruit Load List Contains:")
 #streamlit.text(my_data_row)
 
-streamlit.header("The Fruit Load List in  Contains:")
-streamlit.dataframe(my_data_row)
+#streamlit.header("The Fruit Load List in  Contains:")
+#streamlit.dataframe(my_data_row)
 
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchall()
-streamlit.header("The Fruit Load List DataFrame Contains:")
-streamlit.dataframe(my_data_row)
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+#my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+#my_data_row = my_cur.fetchall()
+#streamlit.header("The Fruit Load List DataFrame Contains:")
+#streamlit.dataframe(my_data_row)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 
 #streamlit.write('Thanks for Adding', add_my_fruit)
 #my_cur.execute("insert into fruit_load_list values('farom streamlit')")
