82e7bc6ece12921662122a09327009c8315063b0
omairi <47171006+omairi@users.noreply.github.com>
2023-03-13 22:21:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,9 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+my_data_row = my_cur.fetchone()
+streamlit.text("The Fruit Load List Contains:")
+streamlit.text(my_data_row)
+
+
