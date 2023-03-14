a5d770edb8801c9d9a74806aafb8b527245db719
omairi <47171006+omairi@users.noreply.github.com>
2023-03-13 23:16:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
 # write your own comment - what does this do?
-@Snowflake related function
+#Snowflake related function
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
            my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
