0314d0b88f6fbfcd757f7f2805015bf7a4aaad21
omairi <47171006+omairi@users.noreply.github.com>
2023-03-13 22:29:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fuityvice aspi response
 streamlit.header("Fruityvice Fruit Advice!")
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi','jackfruit')
-fruit_choice = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['kiwi','jackfruit'])
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#fruit_choice = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['kiwi','jackfruit'])
 
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
