3c717af3d7ef83705a82aa7d6e093062291eb4f8
omairi <47171006+omairi@users.noreply.github.com>
2023-03-13 23:16:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,21 +54,31 @@ streamlit.write('The user entered ', fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
+          
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
 
 # write your own comment - what does this do?
+@Snowflake related function
+def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+           return my_cur.fetchall()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit Load List Contains:")
-streamlit.text(my_data_row)
+# Add a button to load the fruit
+if streamlit.button('Get fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows= get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+     
+#my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+#my_data_row = my_cur.fetchone()
+#streamlit.text("The Fruit Load List Contains:")
+#streamlit.text(my_data_row)
 
 streamlit.header("The Fruit Load List in  Contains:")
 streamlit.dataframe(my_data_row)
